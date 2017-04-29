f68d5970f2cb54b04a0f4f37d124770b6fb18aed
amy963 <crystlliao@hotmail.com>
2017-04-29 22:49:04 +0200

    Added buttons to Reservation, "New" Button does not function yet Changed Customer with Datagrid

--- Hotel_Project/UserControls/Reservation.xaml.cs
+++ Hotel_Project/UserControls/Reservation.xaml.cs
@@ -33,9 +33,55 @@ namespace Hotel_Project
             
         }
 
-        private void Edit_Click(object sender, RoutedEventArgs e)
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int i = hotel.SaveChanges();
+                fehler.Text = i + " rows affected";
+            }
+            catch (Exception e1)
+            {
+                fehler.Text = e1.Message; //zeige alle inner Exception
+                for (var ex = e1.InnerException; ex != null; ex = ex.InnerException)
+                {
+                    fehler.Text = fehler.Text + " / " + ex.Message;
+                }
+
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
         {
 
+            reservation r = (reservation)lireservation.SelectedItem;
+            if(r != null)
+            {
+                hotel.reservation.Remove(r);
+                lireservation.Items.Refresh();
+
+                Save_Click(sender, null);
+            }
+
+            
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            //reservation r = (reservation)lireservation.SelectedItem;
+            //customer c = new customer();
+            //c.Customer_Name = "New";
+            //r.customer.reservation.Add(r);
+            //lireservation.Items.Refresh();
+
+            //klassen k = (klassen)liklassen.SelectedItem;
+            //schueler s = new schueler();
+            //s.S_Name = "NEU";
+            ////s.S_K_Klasse = k.K_ID;
+            //k.schueler2.Add(s);
+
+            //lischueler.Items.Refresh();
         }
     }
 }
--- Hotel_Project/sysdiagrams.cs
+++ Hotel_Project/sysdiagrams.cs
@@ -12,9 +12,12 @@ namespace Hotel_Project
     using System;
     using System.Collections.Generic;
     
-    public partial class adminUsers
+    public partial class sysdiagrams
     {
-        public string Admin_name { get; set; }
-        public string Admin_Password { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
