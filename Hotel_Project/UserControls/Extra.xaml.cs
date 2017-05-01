f47e6ca03b8065461227a81a77527c01d44310b9
amy963 <crystlliao@hotmail.com>
2017-05-01 21:46:48 +0200

    Changed Extra with Datagrid

--- Hotel_Project/UserControls/Extra.xaml.cs
+++ Hotel_Project/UserControls/Extra.xaml.cs
@@ -34,48 +34,70 @@ namespace Hotel_Project
         {
             var erg = ht.extra;
             erg.Load();
-            liextra.ItemsSource = erg.Local.OrderBy(l => l.Extra_Name);
+            sg1.ItemsSource = erg.Local;
+            //liextra.ItemsSource = erg.Local.OrderBy(l => l.Extra_Name);
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
+        private void save1_Click_1(object sender, RoutedEventArgs e)
         {
+            //try
+            //{
+            //    int i = ht.SaveChanges();
+            //    fehler.Text = i + " row(s) affected";
+            //}
+            //catch (Exception e1)
+            //{
+            //    fehler.Text = e1.Message; //zeige alle inner Exception
+            //    for (var ex = e1.InnerException; ex != null; ex = ex.InnerException)
+            //    {
+            //        fehler.Text = fehler.Text + " / " + ex.Message;
+            //    }
+
+            //}
+
             try
             {
-                int i = ht.SaveChanges();
-                fehler.Text = i + " row(s) affected";
+                var anz = ht.SaveChanges();
+                fehler.Text = String.Format("{0} row(s) affected", anz);
             }
             catch (Exception e1)
             {
-                fehler.Text = e1.Message; //zeige alle inner Exception
-                for (var ex = e1.InnerException; ex != null; ex = ex.InnerException)
-                {
-                    fehler.Text = fehler.Text + " / " + ex.Message;
-                }
 
+                fehler.Text = e1.Message + " " +
+                    (e1.InnerException != null ? e1.InnerException.Message : "") + " " +
+                    (e1.InnerException != null && e1.InnerException.InnerException != null ? e1.InnerException.InnerException.Message : "");
             }
         }
 
-        private void Delete_Click(object sender, RoutedEventArgs e)
+        private void sg1_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
-            extra ex = (extra)liextra.SelectedItem;
-            if (ex != null)
-            {
-                ht.extra.Remove(ex);
-                liextra.Items.Refresh();
-
-                Save_Click(sender, null);
-            }
+            // jedes Mal, wenn eine Spalte im DataGrid autogneriert wird
+            if (!e.PropertyName.Contains("_"))   // Navigational Properties nicht anzeigen  (haben kein _ im Namen
+                e.Cancel = true;
+            e.Column.Header = e.PropertyName.Substring(0);
         }
 
-        private void New_Click(object sender, RoutedEventArgs e)
-        {
-            //booking b = (booking)libooking.SelectedItem;
-            //customer c = new customer();
-            //b.Booking_ID += 1;
-            //c.Customer_Name = "New";
-            //ht.booking.Add(b);
-            //ht.customer.Add(c);
-            //libooking.Items.Refresh();
-        }
+        //private void Delete_Click(object sender, RoutedEventArgs e)
+        //{
+        //    extra ex = (extra)liextra.SelectedItem;
+        //    if (ex != null)
+        //    {
+        //        ht.extra.Remove(ex);
+        //        liextra.Items.Refresh();
+
+        //        Save_Click(sender, null);
+        //    }
+        //}
+
+        //private void New_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //booking b = (booking)libooking.SelectedItem;
+        //    //customer c = new customer();
+        //    //b.Booking_ID += 1;
+        //    //c.Customer_Name = "New";
+        //    //ht.booking.Add(b);
+        //    //ht.customer.Add(c);
+        //    //libooking.Items.Refresh();
+        //}
     }
 }
